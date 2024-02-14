2d16d4b0bf93da07dee1887350e6cabeb2edb464
ChrisHanshia16 <hanshia@agilecybersolutions.com>
2024-02-14 15:25:03 +0530

    html

--- summarize_video/views.py
+++ summarize_video/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
 
-def members(request):    
-    return HttpResponse("Hello world!")
+def members(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
+# def members(request):    
+#     return HttpResponse("Hello world!cxh")
